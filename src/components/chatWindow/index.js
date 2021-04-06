9ecc80430419da26ad0d8b15d42237466d44eee5
unknown <pess.igosaldanha@gmail.com>
2021-04-05 23:02:45 -0300

    Add emojji-picker

--- src/components/chatWindow/index.js
+++ src/components/chatWindow/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React,  { useState } from 'react';
+import EmojiPicker from 'emoji-picker-react';
+
 import './style.css'
 
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -9,6 +11,21 @@ import MicIcon from '@material-ui/icons/Mic';
 import CloseIcon from '@material-ui/icons/Close';
 
 export default () => {
+
+    const [emojiOpen, setEmojiOpen] = useState(false)
+
+    // AREA DE FUNÇÕES
+
+    const handleEmojiClick = () =>{
+
+    }
+    const handleOpenEmoji = () =>{
+        setEmojiOpen(true);
+    }
+    const handleCloseEmoji = () =>{
+        setEmojiOpen(false);
+    }
+
   return (
     <div className="chatWindow">
        <div className="chatWindow-header">
@@ -27,17 +44,40 @@ export default () => {
 
        <div className="chatWindow-body"></div>
 
+       <div 
+            className="chatWindow-emojiarea" 
+            style={{height: emojiOpen ? '320px' : '0px'}}
+        >
+                <EmojiPicker
+                    onEmojiClick={handleEmojiClick}
+                    disableSearchBar
+                    disableSkinTonePicker
+                />    
+       </div>
+
        <div className="chatWindow-footer">
 
             <div className="chatWindow-before">
-                <div className="chatWindow-button">
-                    <InsertEmoticonIcon style={{ fontSize: 30 }}/>
-                </div>  
+                
+                <div 
+                    className="chatWindow-button"
+                    onClick={handleCloseEmoji}
+                    style={{width: emojiOpen?40:0}}
+                >
+                    <CloseIcon style={{ fontSize: 30 }}/>
+                </div>
+
+                <div 
+                    className="chatWindow-button"
+                    onClick={handleOpenEmoji}
+                >
+                    <InsertEmoticonIcon style={{ fontSize: 30, color: emojiOpen? '43B581':''}}/>
+                </div>
+
                 <div className="chatWindow-button">
                     <AddPhotoAlternateIcon style={{ fontSize: 30 }}/>
                 </div>  
             </div>
-
             <div className="chatWindow-input-area">
                 <input className="chatWindow-input" type="text" placeholder="Digite uma mensagen"/>
             </div>
