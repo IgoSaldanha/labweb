cade69ea816ef500fec3fd46c533bfb41409e878
unknown <pess.igosaldanha@gmail.com>
2021-04-12 12:56:43 -0300

    stylization of the profile screen in progress

--- src/components/profileWindow/index.js
+++ src/components/profileWindow/index.js
@@ -1,15 +1,57 @@
 import React, { useState } from 'react';
 import './style.css';
 
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
 export default (showProfile) => {
 
-  const showp = showProfile;
 
-  return (
-    <div className="profileWindow">
-      <div>
-        {console.log(showp)}
-      </div>
-    </div>
-  );
+
+    return (
+        <div className="profileWindow" style={{ left: showProfile ? '25%' : 3000 }}>
+
+            <div className="profile-window-config">
+                <div className="hearder-profile">
+                    <div className="header-profile-info">
+                        <img clasName="profile-avatar" src="https://avatars.githubusercontent.com/u/59894220?s=60&v=4" alt=""/>
+                        <p>Igo Saldanha</p>
+                    </div>
+                    <div className="header-profile-more">
+                        <MoreHorizIcon style={{ fontSize: 30 }}/>
+                    </div>
+                </div>
+
+                <div className="content-profile">
+
+                    <div className="content-profile-items">
+                        <div className="content-info">
+                            <p>Código de convite</p>
+                            <div>10290373</div>
+                        </div>
+                        <div className="button-profile">Copiar</div>
+                    </div>
+
+                    <div className="content-profile-items">
+                        <div className="content-info">
+                            <p>Usuario</p>
+                            <div>Igo Saldanha</div>
+                        </div>
+                        <div className="button-profile">Editar</div>
+                    </div>
+
+                    <div className="content-profile-items">
+                        <div className="content-info">
+                            <p>Email</p>
+                            <div>igo@gmail.com</div>
+                        </div>
+                        <div className="button-profile">Editar</div>
+                    </div>
+
+                </div>
+            </div>
+            <div className="remove-account">
+
+            </div>
+        </div>
+    );
 }
